b7c3d1509ba82736e61aa9e57399a03a7b40dcd1
老白 <2044724495@qq.com>
2023-03-07 17:36:20 +0800

    first

--- src/main/java/com/home/FutrueThreadPoolDemo.java
+++ src/main/java/com/home/FutrueThreadPoolDemo.java
@@ -5,7 +5,7 @@ import java.util.concurrent.*;
 public class FutrueThreadPoolDemo
 {
     public static void main(String[] args) throws ExecutionException, InterruptedException {
-//        this is hot-fix test
+//        push
 //        m1();
         long startTime = System.currentTimeMillis();
         ExecutorService executorService = Executors.newFixedThreadPool(3);
